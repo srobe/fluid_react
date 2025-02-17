26d6b6de2f6dab68ac706a6676c48ecfb93f7120
Danya Baron <danyabaron0@gmail.com>
2025-02-17 11:23:59 -0500

    added some fixes to the nav bar sizing including the nasa logo

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -28,13 +28,13 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-black p-5 flex flex-row w-full h-fit border-b justify-center border-white z-50 relative ">
-      <div className=" w-full flex justify-between items-center">
+    <nav className="bg-black p-5 w-screen h-fit border-b border-white z-50 relative ">
+      <div className="flex justify-between items-center w-full px-6">
         
         {/* LOGO */}
-        <div className=" items-center">
+        <div className="flex items-center">
           <Link to='/'>
-            <img src={NasaLogo} alt="NASA Logo" className="h-10 mr-3 shrink-0 w-auto object-contain" />
+            <img src={NasaLogo} alt="NASA Logo" className="max-h-12 w-auto object-contain"/>
           </Link>
         </div>
 
@@ -46,39 +46,39 @@ const Navbar = () => {
         </div>
 
         {/* NAVIGATION LINKS */}
-        <ul className={`md:flex justify-end md:space-x-6 w-full h-full text-white items-center md:text-sm sm:text-xs font-helvetica  ${mobileMenuOpen ? 'block' : 'hidden'}`}>
+        <ul className={`md:flex justify-end md:space-x-6 w-full h-full text-white font-helvetica text-sm ${mobileMenuOpen ? 'block' : 'hidden'}`}>
           
           {/* Home */}
-          <li className="relative group w-fit">
+          <li className="relative group ">
             <Link to="/" className="flex items-center hover:text-blue-400 w-full md:w-auto">
               Home 
             </Link>
           </li>
           {/* Weather Forecasts */}
-          <li className="relative group w-fit text-nowrap" onMouseEnter={() => handleMouseEnter('weather')} onMouseLeave={handleMouseLeave}>
+          <li className="relative group w-fit" onMouseEnter={() => handleMouseEnter('weather')} onMouseLeave={handleMouseLeave}>
             <Link to="/weather-forecasts" className="flex items-center hover:text-blue-400 w-full md:w-auto ">
               Weather Maps <FaCaretDown className="ml-1 inline" />
             </Link>
-            <ul className={`bg-black mt-1 p-6 space-y-2 w-full md:w-52 md:absolute md:bg-black  md:space-y-2
+            <ul className={`bg-black mt-1 p-4 space-y-2 w-full md:w-52 md:absolute md:bg-black md:p-4 md:space-y-2
             transition-opacity duration-300 ease-in-out 
             ${hoveredMenu === 'weather' ? 'opacity-100' : 'opacity-0'}`}>
               <li><a href="/weather-forecasts/datagrams" className="hover:text-blue-400 whitespace-nowrap">Datagrams</a></li>
               <li><a href="/weather-forecasts/weather-maps" className="hover:text-blue-400 whitespace-nowrap">WxMaps</a></li>
-              <li><a href="/weather-forecasts/observing-system-statistics" className="hover:text-blue-400 text-wrap whitespace-nowrap">Observing System Statistics</a></li>
+              <li><a href="/weather-forecasts/observing-system-statistics" className="hover:text-blue-400 whitespace-nowrap">Observing System Statistics</a></li>
               <li><a href="/weather-forecasts/radiance-monitoring" className="hover:text-blue-400 whitespace-nowrap">Radiance Monitoring</a></li>
             </ul>
           </li>
           
           {/* Aerosol & Gas Forecasts */}
-          <li className="relative group w-fit text-nowrap" onMouseEnter={() => handleMouseEnter('aerosol')} onMouseLeave={handleMouseLeave}>
+          <li className="relative group" onMouseEnter={() => handleMouseEnter('aerosol')} onMouseLeave={handleMouseLeave}>
             <Link to="/aerosol-gas" className="flex items-center hover:text-blue-400 w-full md:w-auto">
               Aerosol & Gas Forecasts <FaCaretDown className="ml-1" />
             </Link>
-            <ul className={`bg-black mt-1 p-6 space-y-2 w-full md:w-64 md:absolute md:bg-black md:space-y-2 transition-opacity duration-300 ease-in-out ${hoveredMenu === 'aerosol' ? 'opacity-100' : 'opacity-0'}`}>
+            <ul className={`bg-black mt-1 p-4 space-y-2 w-full md:w-64 md:absolute md:bg-black md:p-4 md:space-y-2 transition-opacity duration-300 ease-in-out ${hoveredMenu === 'aerosol' ? 'opacity-100' : 'opacity-0'}`}>
               <li className="text-gray-400 whitespace-nowrap">Forward Processing</li>
-              <li><a href="/aerosol-gas/datagrams" className="hover:text-blue-400 pr-5  whitespace-nowrap">Datagrams (Aerograms)</a></li>
-              <li><a href="/aerosol-gas/chem-maps" className="hover:text-blue-400 pr-5 flex text-wrap whitespace-nowrap">2D Chem (Atmospheric Composition)</a></li>
-              <li><a href="/aerosol-gas/chem-maps" className="hover:text-blue-400 flex text-wrap whitespace-nowrap">3D Chem (Atmospheric Composition)</a></li>
+              <li><a href="/aerosol-gas/datagrams" className="hover:text-blue-400 whitespace-nowrap">Datagrams (Aerograms)</a></li>
+              <li><a href="/aerosol-gas/chem-maps" className="hover:text-blue-400 whitespace-nowrap">2D Chem (Atmospheric Composition)</a></li>
+              <li><a href="/aerosol-gas/chem-maps" className="hover:text-blue-400 whitespace-nowrap">3D Chem (Atmospheric Composition)</a></li>
               <li className="text-gray-400 whitespace-nowrap">Composition Forecast</li>
               <li><a href="/aerosol-gas/surface-concentrations" className="hover:text-blue-400 whitespace-nowrap">Surface Concentrations</a></li>
               <li><a href="/aerosol-gas/total-column" className="hover:text-blue-400 whitespace-nowrap">Total Column</a></li>
@@ -87,13 +87,13 @@ const Navbar = () => {
 
           {/* Seasonal Prediction */}
           <li>
-            <Link to="/seasonal-prediction" className="w-fit text-nowrap hover:text-blue-400 block md:inline-block">
+            <Link to="/seasonal-prediction" className="hover:text-blue-400 block md:inline-block">
               Seasonal Prediction
             </Link>
           </li>
           
           {/* Reanalysis */}
-          <li className="relative group w-fit text-nowrap" onMouseEnter={() => handleMouseEnter('reanalysis')} onMouseLeave={handleMouseLeave}>
+          <li className="relative group" onMouseEnter={() => handleMouseEnter('reanalysis')} onMouseLeave={handleMouseLeave}>
             <Link to="/reanalysis" className="flex items-center hover:text-blue-400 w-full md:w-auto">
               Reanalysis <FaCaretDown className="ml-1" />
             </Link>
@@ -110,11 +110,11 @@ const Navbar = () => {
           </li>
 
           {/* Mission Support */}
-          <li className="relative group w-fit text-nowrap" onMouseEnter={() => handleMouseEnter('mission')} onMouseLeave={handleMouseLeave}>
+          <li className="relative group" onMouseEnter={() => handleMouseEnter('mission')} onMouseLeave={handleMouseLeave}>
             <Link to="/mission-support" className="flex items-center hover:text-blue-400 w-full md:w-auto">
               Mission Support <FaCaretDown className="ml-1" />
             </Link>
-            <ul className={`bg-black mt-1 p-6 space-y-2 w-full md:w-48 md:absolute md:bg-black md:space-y-2 transition-opacity duration-300 ease-in-out ${hoveredMenu === 'mission' ? 'opacity-100' : 'opacity-0'}`}>
+            <ul className={`bg-black mt-1 p-4 space-y-2 w-full md:w-48 md:absolute md:bg-black md:p-4 md:space-y-2 transition-opacity duration-300 ease-in-out ${hoveredMenu === 'mission' ? 'opacity-100' : 'opacity-0'}`}>
               <li className="text-gray-400 whitespace-nowrap">Active</li>
               <li><a href="#" className="hover:text-blue-400 whitespace-nowrap">AGEST</a></li>
               <li><a href="#" className="hover:text-blue-400 whitespace-nowrap">BLUEFLUX</a></li>
